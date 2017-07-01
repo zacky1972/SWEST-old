bfe8873bb9f06a75d3ef73c40512555b694df297
Susumu Yamazaki <zacky@kitakyu-u.ac.jp>
2017-07-01 20:34:04 +0000

    fix #2

--- app/models/presenter.rb
+++ app/models/presenter.rb
@@ -1,12 +1,6 @@
 class Presenter < ActiveRecord::Base
     validates :name, presence: true
-<<<<<<< HEAD
-=======
     validates :affiliation, presence: true
-<<<<<<< HEAD
->>>>>>> bcdc9e0... add presenter
-=======
     has_attached_file :avatar, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "empty.jpg"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
->>>>>>> 8d43691... add Paperclip & avatar
 end
