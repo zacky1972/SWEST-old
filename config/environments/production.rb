6a668a21d0e5dd16a5d645546cb2c24dffe62893
Susumu Yamazaki <zacky@kitakyu-u.ac.jp>
2017-07-02 11:24:59 +0900

    performance tuning

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Do not expire cache 
+  config.static_cache_control = "public, max-age=31536000"
 end
